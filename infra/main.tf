f5a6b97b18552f1322ee340626c0228d19cde87f
amirchaaari <amirchaari629@gmail.com>
2025-07-18 16:04:31 +0100

    added infrastructure terraform main.tf

---
+++ infra/main.tf
@@ -0,0 +1,56 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group for AKS
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-test-rg"
+  location = "East US"  # Choose closest region to reduce latency
+}
+
+# Log Analytics Workspace for monitoring
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "aks-log-ws"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-test-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-test"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2                            # 👈 Two nodes for minimal cost and redundancy
+    vm_size    = "Standard_B2s"               # 👈 Low-cost VM type
+  }
+
+  identity {
+    type = "SystemAssigned"                   # 👈 AKS manages its own identity
+  }
+
+  # Monitoring via Azure Monitor
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+    }
+  }
+
+  # Kubernetes version (optional, can use latest)
+  kubernetes_version = "1.29.2"               # 👈 Can be left out to use default/stable
+  role_based_access_control_enabled = true
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+# Output kubeconfig command
+output "kube_admin_config_command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.rg.name} --name ${azurerm_kubernetes_cluster.aks.name} --admin"
+}
